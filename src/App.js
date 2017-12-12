e8e5fd8b56abe87959a2b87c7a5f255e493f276a
calebharnell <calebharnell@hotmail.com>
2017-12-12 10:46:15 +1000

    Add links

--- src/App.js
+++ src/App.js
@@ -14,8 +14,16 @@ class App extends Component {
 
           <p>Welcome to my website</p>
 
+          <strong>Navigation: </strong>
+          <Link to="/">Home</Link> |
+          <Link to="/hello"> Hello</Link> |
+          <Link to="/goodbye"> Goodbye</Link> |
+          <Link to="/contact"> Contact</Link>
+
+
           <Route path="/hello" component={Hello} />
           <Route path="/goodbye" component={Goodbye} />
+          <Route path="/contact" component={Contact} />
 
         </div>
       </Router>
@@ -29,6 +37,13 @@ const Hello = () => (
 
 const Goodbye = () => (
   <p>Goodbye cruel World!</p>
+)
+
+const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+    <p>Email: test@test.com</p>
+  </div>
 )
 
 export default App;
